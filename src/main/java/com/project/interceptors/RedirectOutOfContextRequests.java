20f05ceace77f445877e878166e77c2ecfc84e16
CoderAniket <aniketnangare87@gmail.com>
2024-11-29 14:31:06 +0530

    enabled virtual threads

--- src/main/java/com/project/interceptors/RedirectOutOfContextRequests.java
+++ src/main/java/com/project/interceptors/RedirectOutOfContextRequests.java
@@ -1,20 +1,47 @@
-//package com.project.interceptors;
-//
-//
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//
-//
-//public class RedirectOutOfContextRequests implements HandlerInterceptor {
-//
-//    Logger logger = LoggerFactory.getLogger(RedirectOutOfContextRequests.class);
-//
-//    @Override
-//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-////        logger.info("{} {}", request.getContextPath(), request.getRequestURL());
-//
-//        response.sendRedirect("/movie");
-//        return false;
-//
-//    }
-//}
+package com.project.interceptors;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+import reactor.util.annotation.NonNullApi;
+
+import java.net.URI;
+
+@Component
+
+public class RedirectOutOfContextRequests implements WebFilter {
+
+
+    Logger logger = LoggerFactory.getLogger(RedirectOutOfContextRequests.class);
+
+
+
+
+    @Override
+
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+
+        logger.info("inside the filter {}", getClass());
+
+        var request =  exchange.getRequest()  ; var response = exchange.getResponse();
+        logger.info("{}",request.getURI().getPath());
+        if(!request.getURI().getPath().startsWith("/movie") && !request.getURI().getPath().startsWith("/repo")){
+            logger.info("WILL REDIRECT OUT OF CONTEXT REQUEST");
+
+            response.setStatusCode(HttpStatusCode.valueOf(302));
+            response.getHeaders().setLocation(URI.create("/movie"));
+
+            return response.setComplete();
+
+        }
+        return chain.filter(exchange);
+
+
+    }
+}
